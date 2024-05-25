5913fe6c9046c56033192ecc3c7b9b8044c5ad97
rina-333 <rina.career.s@gmail.com>
2024-05-25 22:52:09 +0700

    Push All

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container-fluid h-100 p-0">
+    <div class="row g-0 h-100">
+      <div class="col-8 h-100">
+        <div class="large-image">
+          <swiper
+          style="height: 100%"
+            :modules="modules"
+            :autoplay="{delay: 3000, stopOnLastSlide: false}"
+          >
+          <swiper-slide v-for="(image, index) in largeImages" :key="index">
+              <img :src="image" class="img-fluid" alt="Slide 1">
+            </swiper-slide>
+          </swiper>
+        </div>
+      </div>
+      <div class="col-4 d-flex flex-column">
+        <div class="small-image-top" v-for="(image, index) in smallImages" :key="index">
+          <img :src="image" class="img-fluid" alt="Small Responsive image">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { reactive, ref } from 'vue'
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { A11y, Autoplay } from 'swiper/modules';
+
+import 'swiper/css';
+
+export default{
+  components: {
+    Swiper,
+    SwiperSlide
+  },
+  setup() {
+    const largeImages = ref([
+      'https://fakeimg.pl/1600x1200',
+      'https://fakeimg.pl/1600x1200',
+      'https://fakeimg.pl/1600x1200',
+    ])
+
+    const smallImages = reactive(
+      [
+        'https://fakeimg.pl/800x600',
+        'https://fakeimg.pl/800x600',
+      ]
+    )
+    return {
+      largeImages,
+      smallImages,
+      modules: [A11y, Autoplay],
+    }
+  }
+}
+</script>
+
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import './scss/styles.scss'
+import './style.css'
+import App from './App.vue'
+
+import * as bootstrap from 'bootstrap'
+
+createApp(App).mount('#app')
